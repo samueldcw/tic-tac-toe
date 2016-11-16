dc7e376978615c3364d915b5507f2c25093aa587
Sam clem-whiting <samclemwhiting@gmail.com>
2016-11-16 10:17:46 -0800

    giuhaewreliufon w 348ewnf d0p9ol

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,10 +3,10 @@
 var namespace = "http://www.w3.org/2000/svg"
 function makeShape() {
   //code to create shape
-  var canvas = document.getElementNs("circle")
-  var circle  = document.createElementById(namespace, "game-board")
+  var canvas = document.getElementNs("game-board")
+  var game-board  = document.createElementById(namespace, "game-board")
   circle.setAttribute("cx", 35)
   circle.setAttribute("cy", 15)
   circle.setAttribute("r", 65)
-  canvasappendChild(circle)
+  canvasappendChild(game-board)
 }
